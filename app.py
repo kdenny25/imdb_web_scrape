23c59079d11362a4d3627b4bf4030d0ead84ead6
kdenny <kelsead25@gmail.com>
2022-09-21 15:08:30 -0400

    Now exports data to a csv file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 # GET request for scraping page
 page = requests.get\
@@ -11,18 +12,40 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # find all div containers for movies
 mList = soup.find_all('div', class_='lister-item-content')
 
-titles = []
-pRate = []
-# iterate through containers
-for movie in mList:
-    titles.append(movie.find('h3', class_='lister-item-header').find('a').text)
 
-    # check for missing values and append the parental rating
-    pRate_val = movie.find('span', class_='certificate')
-    if pRate_val != None:
-        pRate.append(movie.find('span', class_='certificate').text)
+
+movies_dict = {'title': [],
+               'pRate': [],
+               'genre': [],
+               'starRate': [],
+               }
+
+# returns text value if it exists
+# otherwise it returns 'None'
+def find_val(parent_soup, container, class_id):
+    value_to_check = parent_soup.find(container, class_=class_id)
+
+    if value_to_check != None:
+        return value_to_check.text
     else:
-        pRate.append('None')
+        return 'None'
 
-print(titles)
-print(pRate)
+# iterate through containers
+for y, movie in enumerate(mList):
+    # extract title
+    movies_dict['title'].append(movie.find('h3', class_='lister-item-header').find('a').text)
+
+    # extract parental rating
+    movies_dict['pRate'].append(find_val(movie, 'span', 'certificate'))
+
+    # extract genre
+    movies_dict['genre'].append(find_val(movie, 'span', 'genre').replace("\n", ''))
+
+    # extract star rating
+    movies_dict['starRate'].append(find_val(movie, 'span', 'ipl-rating-star__rating'))
+
+
+movies_df = pd.DataFrame.from_dict(movies_dict)
+
+movies_df.to_csv('data/movies_data.csv')
+print(movies_df.head())
