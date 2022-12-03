44f3e0a47744f739c4beac3cbfb4d0571b6f9bba
kdenny25 <kelsead25@gmail.com>
2022-12-03 11:50:08 -0500

    Adjusted search page data to include a few items that are more consistent.

--- readme.md
+++ readme.md
@@ -1,15 +1,18 @@
 # Webscraping IMDB list
 
 This is just a simple script to pull data of Disney movies between 2021 and 2022
-from a pre-created list on IMDB.  
+from a pre-created list on IMDB. 
 
 Originally I had a pre-created list of disney titles but for reproducibility 
 scrape_disney_titles.py was created to generate the list of titles. In this 
 file contains a function 'list_disney_releases' requiring the input of a range of years.
 The result is a dataframe containing the Title, Year of Release and the IMDB url tail.
 
-At the time of writing this script the dataset was missing full release dates
-But, having additional information about the films could have a positive
-impact on my analysis. To add more data a new script is born. This script
-searches each title based from the list provided by Finder. I can now search
-through each movie and extract more information.
+Now I have a function that can grab a few additional details about a movie. Most of these 
+are performance measures in addition to the release date. At this point I have identified a
+few challenges. 
+
+1. It is not easy to determine separate movies from tv series as they are not categorized as such. 
+   * I may be able to categorize them myself from the search titles.
+2. Acquiring the Motion Picture Rating is not always in the same spot
+3. 
--- scrape_disney_titles.py
+++ scrape_disney_titles.py
@@ -2,9 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 import pandas as pd
+import re
 
 
 
@@ -24,8 +25,10 @@ def list_disney_releases(start, end):
 
     title_dict = {
         'title': [],
-        'year': [],
-        'url': []
+        'url': [],
+        'certificate': [],
+        'runtime': [],
+        'genre': []
     }
 
     def add_titles_to_dict():
@@ -34,11 +37,36 @@ def list_disney_releases(start, end):
         :return:
         appends itmes to title_dict
         """
-        title_containers = soup.find_all("h3", class_="lister-item-header")
+
+        def does_element_exist(container, class_name):
+            """ checks if element exists in imdb lister item"""
+            try:
+                value = container.find('span', class_=class_name).text
+            except (AttributeError, NameError):
+                value = 'Na'
+            return value
+
+
+        title_containers = soup.find_all('div', class_='lister-item-content')
         for container in title_containers:
-            title_dict['title'].append(container.find('a').text)
-            title_dict['year'].append(container.find('span', class_='lister-item-year text-muted unbold').text)
-            title_dict['url'].append(container.find('a', href=True)['href'])
+            # gather data for title and url
+            header_container= container.find('h3', class_='lister-item-header')
+            title_dict['title'].append(header_container.find('a').text)
+
+            title_dict['url'].append(header_container.find('a', href=True)['href'])
+
+            # the following aren't always available and need conditional functions to work
+            title_dict['certificate'].append(does_element_exist(container, 'certificate'))
+            title_dict['runtime'].append(does_element_exist(container, 'runtime'))
+            title_dict['genre'].append(does_element_exist(container, 'genre').strip())
+
+
+
+        # title_containers = soup.find_all("h3", class_="lister-item-header")
+        # for container in title_containers:
+        #     title_dict['title'].append(container.find('a').text)
+        #     title_dict['year'].append(container.find('span', class_='lister-item-year text-muted unbold').text)
+        #     title_dict['url'].append(container.find('a', href=True)['href'])
 
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -65,13 +93,38 @@ def list_disney_releases(start, end):
     return titles_dataframe
 
 def fill_release_details(file_path):
-    driver = webdriver.Chrome('./driver/chromedriver')
+    """ Cycles through urls from the csv file created using disney releases.
+    Returns a combined dataframe with Rating, number of ratings, popularity score,
+    metascore and the release date.
+
+    :param file_path:
+    :return: DataFrame
+        title
+        url
+        certificate
+        runtime
+        genre
+        rating
+        num_ratings
+        popularity
+        metascore
+        release_date
+    """
+    options = Options()
+    # this function does not work when headless = True
+    options.headless = False
+    driver = webdriver.Chrome('./driver/chromedriver', options=options)
     release_df = pd.read_csv(file_path, index_col='Unnamed: 0')
 
-    new_columns = ['rating', 'num_ratings', 'popularity', 'metascore', 'type',
-                   'length', 'rating', 'genre', 'release_date']
+    temp_dict = {
+        'rating': [],
+        'num_ratings': [],
+        'popularity': [],
+        'metascore': [],
+        'release_date': []
+    }
 
-    release_df[new_columns] = 'Na'
+    #release_df[new_columns] = 'Na'
     print(release_df)
     base_url = 'https://www.imdb.com'
 
@@ -84,47 +137,61 @@ def fill_release_details(file_path):
         return element_to_check
 
     for idx, row in release_df.iterrows():
-        #new_url = base_url + row['url']
-        new_url = 'https://www.imdb.com/title/tt10298840/'
+        new_url = base_url + row['url']
+        #new_url = 'https://www.imdb.com/title/tt10298840/'
         driver.get(new_url)
         print(new_url)
 
         # Get Rating Value
-        rating = if_element_exists('xpath',
+        rating_val = if_element_exists('xpath',
                                        "//div[@data-testid='hero-rating-bar__aggregate-rating__score']/span[1]")
-        rating = float(rating) if rating != 'NaN' else rating
+        rating_val = float(rating_val) if rating_val != 'NaN' else rating_val
 
         # Get number of ratings submitted.
-        num_rating = if_element_exists('xpath',
+        num_rating_val = if_element_exists('xpath',
                                         "//div[@data-testid='hero-rating-bar__aggregate-rating__score']/../div[3]")
-        if num_rating != 'NaN':
-            if 'K' in num_rating:
-                num_rating = float(num_rating.replace('K', '')) * 1000
+        if num_rating_val != 'NaN':
+            if 'K' in num_rating_val:
+                num_rating_val = float(num_rating_val.replace('K', '')) * 1000
             else:
-                num_rating = float(num_rating)
+                num_rating_val = float(num_rating_val)
 
         # Get Popularity
-        popularity = if_element_exists('xpath', "//div[@data-testid='hero-rating-bar__popularity__score']")
-        popularity = int(popularity) if popularity != 'NaN' else popularity
+        popularity_val = if_element_exists('xpath', "//div[@data-testid='hero-rating-bar__popularity__score']")
+        #popularity_val = int(popularity_val) if popularity_val != 'NaN' else popularity_val
 
         # Get metascore
-        metascore = if_element_exists('xpath', "//span[@class='score-meta']")
-        metascore = int(metascore) if metascore != 'NaN' else metascore
+        metascore_val = if_element_exists('xpath', "//span[@class='score-meta']")
+        metascore_val = int(metascore_val) if metascore_val != 'NaN' else metascore_val
 
-        release_date = if_element_exists('xpath', "//li[@data-testid='title-details-releasedate']/div/ul/li/a")
+        release_date_val = if_element_exists('xpath', "//li[@data-testid='title-details-releasedate']/div/ul/li/a")
 
-        if '(United States)' in release_date:
-            release_date = release_date.replace(' (United States)', '')
+        country = re.findall("\(.*?\)", release_date_val)
+        if len(country) > 0:
+            release_date_val = release_date_val.replace(country[0], '').strip()
 
-        print(rating)
-        print(num_rating)
-        print(popularity)
-        print(metascore)
-        print(release_date)
+        print(rating_val)
+        print(num_rating_val)
+        print(popularity_val)
+        print(metascore_val)
+        print(release_date_val)
+
+        temp_dict['rating'].append(rating_val)
+        temp_dict['num_ratings'].append(num_rating_val)
+        temp_dict['popularity'].append(popularity_val)
+        temp_dict['metascore'].append(metascore_val)
+        temp_dict['release_date'].append(release_date_val)
+
+    temp_df = pd.DataFrame(temp_dict)
+    new_df = pd.concat([release_df, temp_df], axis=1)
+
+    return new_df
 
 
 
-fill_release_details('./data/disney_releases.csv')
+
+fill_details = fill_release_details('./data/disney_releases.csv')
+fill_details.to_csv('./data/disney_release_data.csv')
 
 # disney_releases = list_disney_releases('2015', '2022')
 # disney_releases.to_csv('./data/disney_releases.csv')
