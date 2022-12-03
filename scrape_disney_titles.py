121b04805365f601e788cb108a5cf334d1c545e9
kdenny25 <kelsead25@gmail.com>
2022-12-02 20:24:12 -0500

    started using Selenium to navigate through data. Rating, number of ratings, popularity value, metascore and release date are all captured in the new function.

--- scrape_disney_titles.py
+++ scrape_disney_titles.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
-import re
+
 
 
 def list_disney_releases(start, end):
@@ -17,7 +21,6 @@ def list_disney_releases(start, end):
     """
     page_number = 1
     url = f'https://www.imdb.com/search/keyword/?keywords=disney&ref_=kw_ref_yr&sort=release_date,desc&mode=detail&page={str(page_number)}&release_date={start}%2C{end}'
-    search_url = 'https://www.imdb.com/search/keyword/?keywords=disney&ref_=kw_nxt&mode=detail&'
 
     title_dict = {
         'title': [],
@@ -61,6 +64,68 @@ def list_disney_releases(start, end):
     titles_dataframe = pd.DataFrame(title_dict)
     return titles_dataframe
 
-disney_releases = list_disney_releases('2015', '2022')
-disney_releases.to_csv('./data/disney_releases.csv')
-print(disney_releases)
+def fill_release_details(file_path):
+    driver = webdriver.Chrome('./driver/chromedriver')
+    release_df = pd.read_csv(file_path, index_col='Unnamed: 0')
+
+    new_columns = ['rating', 'num_ratings', 'popularity', 'metascore', 'type',
+                   'length', 'rating', 'genre', 'release_date']
+
+    release_df[new_columns] = 'Na'
+    print(release_df)
+    base_url = 'https://www.imdb.com'
+
+    def if_element_exists(by, element):
+        try:
+            element_to_check = driver.find_element(by, element).text
+        except NoSuchElementException:
+            element_to_check = 'NaN'
+
+        return element_to_check
+
+    for idx, row in release_df.iterrows():
+        #new_url = base_url + row['url']
+        new_url = 'https://www.imdb.com/title/tt10298840/'
+        driver.get(new_url)
+        print(new_url)
+
+        # Get Rating Value
+        rating = if_element_exists('xpath',
+                                       "//div[@data-testid='hero-rating-bar__aggregate-rating__score']/span[1]")
+        rating = float(rating) if rating != 'NaN' else rating
+
+        # Get number of ratings submitted.
+        num_rating = if_element_exists('xpath',
+                                        "//div[@data-testid='hero-rating-bar__aggregate-rating__score']/../div[3]")
+        if num_rating != 'NaN':
+            if 'K' in num_rating:
+                num_rating = float(num_rating.replace('K', '')) * 1000
+            else:
+                num_rating = float(num_rating)
+
+        # Get Popularity
+        popularity = if_element_exists('xpath', "//div[@data-testid='hero-rating-bar__popularity__score']")
+        popularity = int(popularity) if popularity != 'NaN' else popularity
+
+        # Get metascore
+        metascore = if_element_exists('xpath', "//span[@class='score-meta']")
+        metascore = int(metascore) if metascore != 'NaN' else metascore
+
+        release_date = if_element_exists('xpath', "//li[@data-testid='title-details-releasedate']/div/ul/li/a")
+
+        if '(United States)' in release_date:
+            release_date = release_date.replace(' (United States)', '')
+
+        print(rating)
+        print(num_rating)
+        print(popularity)
+        print(metascore)
+        print(release_date)
+
+
+
+fill_release_details('./data/disney_releases.csv')
+
+# disney_releases = list_disney_releases('2015', '2022')
+# disney_releases.to_csv('./data/disney_releases.csv')
+# print(disney_releases)
